7516686404ed65090d726c8e3ecba3461fd43a27
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2026-01-13 13:04:49 +0530

    chore: add maven central URL for downloading openapi

--- install-specmatic-openapi.ps1
+++ install-specmatic-openapi.ps1
@@ -3,6 +3,7 @@ layout: none
 ---
 {% capture latest_version %}{{ site.specmatic-openapi-version }}{% endcapture %}
 {% capture download_urls %}
+  "https://repo1.maven.org/maven2/io/specmatic/openapi/specmatic-openapi-all/{tool_version}/specmatic-openapi-all-{tool_version}.jar"
   "https://repo.specmatic.io/releases/io/specmatic/openapi/specmatic-openapi-all/{tool_version}/specmatic-openapi-all-{tool_version}.jar"
 {% endcapture %}
 
--- install-specmatic-openapi.sh
+++ install-specmatic-openapi.sh
@@ -3,6 +3,7 @@ layout: none
 ---
 {% capture tool_version %}{{ site.specmatic-openapi-version }}{% endcapture %}
 {% capture download_urls %}
+  "https://repo1.maven.org/maven2/io/specmatic/openapi/specmatic-openapi-all/{{ site.specmatic-openapi-version }}/specmatic-openapi-all-{{ site.specmatic-openapi-version }}.jar"
   "https://repo.specmatic.io/releases/io/specmatic/openapi/specmatic-openapi-all/{{ site.specmatic-openapi-version }}/specmatic-openapi-all-{{ site.specmatic-openapi-version }}.jar"
 {% endcapture %}
 
