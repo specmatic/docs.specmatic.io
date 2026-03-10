8df9d7f85a0f56c163f29b1dc276881fc51d6a77
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2026-03-10 14:43:23 +0530

    chore: fix the "Edit this page" link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,7 +96,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/specmatic/specmatic-documentation',
+            'https://github.com/specmatic/docs.specmatic.io',
         },
         theme: {
           customCss: './src/css/custom.css',
